70b5f97a8e14353f6cc3eee14f87a672a6a1e54a
Watcharapong Suksrisawangwong <pong8aor@gmail.com>
2024-02-25 00:23:51 +0700

    Added basic-auth security config

---
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/securityconfig/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.kbtg.bootcamp.posttest.securityconfig;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((request) -> request
+                        .requestMatchers("/admin/lotteries").authenticated()
+                        .anyRequest().permitAll())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}
